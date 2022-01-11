e051ddd3070c21f354b6a11a90c93b457aa2a74c
Alberto Barbosa 021 <85910024+Alberto21-boop@users.noreply.github.com>
2022-01-11 13:25:46 -0300

    Add files via upload

---
+++ estudo1/estudo1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace estudo1
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            bool a = 3 > 5 && 10 != 9;
+            bool b = 3 < 5 || 10 > 9;
+
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+        }
+    }
+}
